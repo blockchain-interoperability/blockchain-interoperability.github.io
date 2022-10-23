1a0b17d9fb9fe97fa93c911df771b7ca2d374c7f
inwonakng <inwon.kang04@gmail.com>
2022-10-23 00:44:28 -0400

    fixed default path. attakcs view for now

--- src/layouts/MainPage/MainPage.js
+++ src/layouts/MainPage/MainPage.js
@@ -125,7 +125,7 @@ function MainPage(props) {
               />
               <Switch>
                 {getRoutes(routes)}
-                <Redirect from="*" to="/dashboard" />
+                <Redirect from="*" to="/attacks" />
               </Switch>
               {
                 // we don't want the Footer to be rendered on map page
